7f31099d655258e03da67f86e8623fc5090a52ad
David Williamson <david.williamson@varilink.co.uk>
2023-05-25 17:58:49 +0100

    THEME CUSTOMISATIONS VIA THE CHILD THEME
    Some customisations that can't be actioned via a theme.json file and so
    are done via the child theme stylesheet instead:
    
     - Centre align caption elements
    
     - Put a drop shadow on h1 elements
    
    Modified child theme functions.php so that this child theme styleshee is
    enqueued accordingly.
    
    Created NPM package dependencies files to include fontawesome icons and
    enqueued these via the modified theme functions.php too.git

--- theme/functions.php
+++ theme/functions.php
@@ -1,8 +1,22 @@
 <?php
+
 /* Enqueue Styles */
-if ( ! function_exists('thr_enqueue_styles') ) {
-    function thr_enqueue_styles() {
-        wp_enqueue_style( 'twenty-twenty-three-style', get_template_directory_uri() .'/style.css' );
-    }
-    add_action('wp_enqueue_scripts', 'thr_enqueue_styles');
+
+function thr_enqueue_styles() {
+
+    /* My child theme styleshee */
+    wp_enqueue_style(
+        'twenty-twenty-three-child-style',
+        get_stylesheet_directory_uri() .'/style.css'
+    );
+
+    /* Fontawesome icons */
+    wp_enqueue_style(
+        'font-awesome',
+        get_stylesheet_directory_uri() .
+        '/font-awesome/css/all.min.css'
+    );
+
 }
+
+add_action('wp_enqueue_scripts', 'thr_enqueue_styles');
