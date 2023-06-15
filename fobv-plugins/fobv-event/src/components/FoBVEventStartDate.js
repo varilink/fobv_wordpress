b90166b09c2a0d1ed4628e8c3d3b26015e0a4fbf
David Williamson <david.williamson@varilink.co.uk>
2023-06-15 20:23:56 +0100

    SHOW THE DATE PICKER FOR THE FOBV EVENT START DATE VIA A DROPDOWN
    I've copied this from the approach that WordPress itself uses for the
    publish date. You have a hyperlink showing the publish date, which opens
    a date picker dropdown if you click on it that allows you to change that
    date.

--- fobv-plugins/fobv-event/src/components/FoBVEventStartDate.js
+++ fobv-plugins/fobv-event/src/components/FoBVEventStartDate.js
@@ -2,28 +2,72 @@ import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
-import { PanelRow, DatePicker } from '@wordpress/components';
+import { Button, Dropdown, PanelRow, DatePicker } from '@wordpress/components';
+
+function display_date(date_in) {
+
+	let string_out = 'You must select a start date';
+
+	if (date_in) {
+
+		string_out =
+			date_in.substring(8, 10) + '-' +
+			date_in.substring(5, 7) + '-' +
+			date_in.substring(0, 4);
+
+	}
+
+	return string_out;
+
+}
 
 const FoBVEventStartDate = ( { postType, metaFields, setMetaFields } ) => {
 
 	if ( 'fobv-event' !== postType ) return null;
 
-	return(
-		<PluginDocumentSettingPanel 
+	return (
+
+		<PluginDocumentSettingPanel
 			title={ __( 'Start date' ) } 
 			icon="calendar"
 			initialOpen={ true }
 		>
+
 			<PanelRow>
-				<DatePicker 
-                    currentDate={ metaFields._fobv_event_start_date }
-                    onChange={ ( newDate ) => setMetaFields( { _fobv_event_start_date: newDate } ) }
-                    __nextRemoveHelpButton
-                    __nextRemoveResetButton
+
+				<Dropdown
+					renderToggle={ ( { isOpen, onToggle } ) => (
+
+						<Button
+							variant='link'
+							onClick={ onToggle }
+							aria-expanded={ isOpen }
+						>
+							{display_date(metaFields._fobv_event_start_date)}
+						</Button>
+
+					)}
+					renderContent={ () => (
+						<DatePicker 
+    	        	        currentDate={
+								metaFields._fobv_event_start_date | null
+							}
+        	        	    onChange={
+								( newDate ) =>
+								setMetaFields( {
+									_fobv_event_start_date: newDate
+								} )
+							}
+						/>
+					)}
 				/>
+
 			</PanelRow>
+
 		</PluginDocumentSettingPanel>
-	);
+
+	)
+
 }
 
 const applyWithSelect = withSelect( ( select ) => {
