0aac7fe3a89e933bd9c2689d3d937dc3c4f12ec1
David Williamson <david.williamson@varilink.co.uk>
2023-06-16 10:24:58 +0100

    ENQUEUE STYLESHEET AND DEFINE CUSTOM QUERY FOR THE EVENT PLUGIN

--- fobv-plugins/fobv-event/fobv-event.php
+++ fobv-plugins/fobv-event/fobv-event.php
@@ -27,6 +27,8 @@ add_action( 'init', function () {
     );    
 } );
 
+// Register the start and end dates for the Event custom post type
+
 add_action( 'init', function () {
 	$metafields = [ '_fobv_event_start_date', '_fobv_event_end_date' ];
 	foreach ( $metafields as $metafield ) {
@@ -45,7 +47,14 @@ add_action( 'init', function () {
 	}
 } );
 
+// Queue the stylesheet and script for the panel we add to the document settings
+// for the Event custom post type to allow entry of the start and end dates.
+
 add_action( 'enqueue_block_editor_assets', function () {
+	wp_enqueue_style(
+        'fobv-event',
+        plugin_dir_url( __FILE__ ) . 'build/index.css',
+	);
     wp_enqueue_script(
         'fobv-event',
         plugin_dir_url( __FILE__ ) . 'build/index.js',
@@ -56,18 +65,45 @@ add_action( 'enqueue_block_editor_assets', function () {
 	);
 } );
 
+// Define the query to apply to posts of the Event post type.
+
+function fobv_event_query ($query) {
+	$query['meta_key'] = '_fobv_event_start_date';
+	$query['orderby'] = 'meta_value';
+	$query['meta_query'] = array(
+		'relation' => 'OR',
+		array(
+			'relation' => 'AND',
+			array(
+				'key' => '_fobv_event_end_date',
+				'value' => ''
+			),
+			array(
+				'key' => '_fobv_event_start_date',
+				'value' => date('Y-m-d'),
+				'type' => 'DATE',
+				'compare' => '>='
+			)
+		),
+		array(
+			'key' => '_fobv_event_end_date',
+			'value' => date('Y-m-d'),
+			'type' => 'DATE',
+			'compare' => '>='
+		)
+	);
+	return $query;
+}
+// Apply Event custom query in the front end.
 add_filter(
-	'rest_fobv-event_query',
-	function ( $args, $request ) {
-
-#		varilink_write_log($args);
-
-		$args['meta_key'] = '_fobv_event_start_date';
-		$args['orderby'] = 'meta_value';
-		return $args;
-
-	},
-	10,
-	2
+	'query_loop_block_query_vars',
+	function ( $query ) {
+		if ( 'fobv-event' === $query['post_type'] ) {
+			return fobv_event_query($query);
+		} else {
+			return $query;
+		}
+	}
 );
-
+// Apply Event custom query in the back end.
+add_filter('rest_fobv-event_query', 'fobv_event_query');
