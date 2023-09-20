a3486231e2c71d7791c13f4a068d8ab457bbb800
David Williamson <david.williamson@varilink.co.uk>
2023-09-20 13:49:56 +0100

    THEME FUNCTION UPDATES
     - Add the fobv-featured-image-header shortcode that displays a header
       containing the featured image if one if provided but nothing if one
       isn't.
    
     - Set a constant to indicate that HTML5 automatic validation should not
       be used. Actually, this was meant to work in conjunction with the
       varilink-forms bit I don't think I've evolved that functionality to
       the point where it is in use yet.

--- theme/functions.php
+++ theme/functions.php
@@ -19,3 +19,34 @@ function thr_enqueue_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'thr_enqueue_styles');
+
+function fobv_featured_image_header () {
+
+    $featured_image_header = <<<'EOD'
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"isDark":false} -->
+<div class="wp-block-cover is-light">
+
+    <span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim">
+    </span>
+
+    <div class="wp-block-cover__inner-container">
+
+        <!-- wp:post-title {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"custom-color-3"} /-->
+
+    </div>
+
+</div>
+<!-- /wp:cover -->
+EOD;
+
+    return has_post_thumbnail() ? $featured_image_header : NULL;
+
+}
+
+add_action( 'init', function () {
+
+    add_shortcode( 'fobv-featured-image-header', 'fobv_featured_image_header' );
+
+} );
+
+define('THEME_NOVALIDATE', TRUE);
